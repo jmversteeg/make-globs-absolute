ab763d03b3c446266afabb21ccda49b1b5e4932a
JM Versteeg <janmerijnversteeg@gmail.com>
2016-06-30 15:28:08 +0200

    Fix test

--- test.js
+++ test.js
@@ -3,10 +3,10 @@ import makeGlobsAbsolute from './';
 
 
 test('Map an array of globs to their absolute path equivalent', t => {
-	process.chdir('/Users');
+	process.chdir('/usr');
 	t.deepEqual(makeGlobsAbsolute([]), []);
-	t.deepEqual(makeGlobsAbsolute(['foo']), ['/Users/foo']);
-	t.deepEqual(makeGlobsAbsolute(['!foo']), ['!/Users/foo']);
-	t.deepEqual(makeGlobsAbsolute(['foo', '!foo/bar']), ['/Users/foo', '!/Users/foo/bar']);
+	t.deepEqual(makeGlobsAbsolute(['foo']), ['/usr/foo']);
+	t.deepEqual(makeGlobsAbsolute(['!foo']), ['!/usr/foo']);
+	t.deepEqual(makeGlobsAbsolute(['foo', '!foo/bar']), ['/usr/foo', '!/usr/foo/bar']);
 	t.deepEqual(makeGlobsAbsolute(['!foo', 'foo/bar'], '/Some/other/dir'), ['!/Some/other/dir/foo', '/Some/other/dir/foo/bar']);
 });
